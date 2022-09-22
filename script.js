367e9a53df2428f15c4a21d8d47757dbc749353f
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-09-22 17:07:20 +0200

    add animation

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+ScrollReveal().reveal('.main-header', { delay: 300, origin:'bottom' });
+ScrollReveal().reveal('.main-p', { delay: 400, origin:'bottom' });
+ScrollReveal().reveal('.g-container', { delay: 500, origin:'bottom' });
+ScrollReveal().reveal('.usd', { delay: 600, origin:'bottom' });
+ScrollReveal().reveal('.main-btn', { delay: 700, origin:'bottom' });
+ScrollReveal().reveal('.main-btnn', { delay: 700, origin:'bottom' });
+ScrollReveal().reveal('.n-usd', { delay: 800, origin:'bottom' });
+ScrollReveal().reveal('.n-btn', { delay: 900, origin:'bottom' });
+ScrollReveal().reveal('.icon-n', { delay: 1000, origin:'bottom' });
+
+
+
+ScrollReveal().reveal('.f-header', { delay: 300, origin:'left' });
+ScrollReveal().reveal('.eco-header', { delay: 300, origin:'left' });
+ScrollReveal().reveal('.e-header', { delay: 300, origin:'bottom' });
+ScrollReveal().reveal('.e-btn', { delay: 400, origin:'bottom' });
+ScrollReveal().reveal('.s-img', { delay: 300, origin:'bottom' });
+ScrollReveal().reveal('.svg-n', { delay: 300, origin:'left' });
+
